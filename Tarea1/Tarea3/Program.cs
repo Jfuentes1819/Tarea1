66b14a903dbb750cd9bb13005e974f07593ed1ec
Jfuentes1819 <jfuentes0619@gmail.com>
2024-06-12 09:20:48 -0400

    TArea 4

--- Tarea1/Tarea3/Program.cs
+++ Tarea1/Tarea3/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 using System.Reflection.Metadata;
-
+    
 Console.WriteLine("Hello!");
 
 int[] ages = { 15, 25, 46, 37, 50 };
@@ -29,9 +29,3 @@ for (int i = 0; i < ages.Length; i++)
 
 Console.ReadKey();
 
-
-//Declare e inicialice un arreglo de enteros con las edades de 5 personas.
-//Declare e inicialice un arreglo de cadenas con los nombres de 5 personas.
-//Itere sobre los arreglos e imprima cada nombre junto con su edad.
-//Defina una función que tome un nombre y una edad como parámetros, y devuelva un saludo personalizado.
-//Llame a esta función para cada par de nombre y edad en los arreglos e imprima el resultado.
---
+++ Tarea1/Tarea4/Program.cs
@@ -0,0 +1,54 @@
+﻿//Sentencias de control avanzado iterativas
+
+//while
+//do while = hazme pero primero pregunta(casi nunca se usa)
+//for
+//foreach
+
+
+using System.Runtime.Intrinsics.X86;
+
+int index = 0;
+int[] numbers = { 13, 15, 48, 50, 62, 15, 19, 4, 69, 50 };
+
+
+int sum = 0;
+
+
+while(index < numbers.Length) //names.Count())
+{
+    Console.WriteLine(numbers[index]);
+    index++;
+}
+
+
+for (int i =1; i <=5; i ++)
+{
+    Console.WriteLine(index);
+    index++;
+
+}
+
+
+
+foreach (int item in numbers)//se puede usar var pero trata de usar el tipo de dato correcto
+{
+    Console.WriteLine(item);
+}
+
+foreach (int item in numbers)//se puede usar var pero trata de usar el tipo de dato correcto
+{
+
+    sum += item;
+    Console.WriteLine(item);
+}
+
+
+
+
+
+
+
+
+
+ 
